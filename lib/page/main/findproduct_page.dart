ebdf611b9c0f43d65ec34e6c4f50df6a3c76c77e
MJ Sidik <darkimanity@gmail.com>
2025-12-04 09:49:36 +0700

    update hari kamis 4 desember 2025

--- lib/page/main/findproduct_page.dart
+++ lib/page/main/findproduct_page.dart
@@ -1,4 +1,7 @@
+import 'package:alchemiststock/widget/widget_batchProduct.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class FindproductPage extends StatefulWidget {
   const FindproductPage({super.key});
@@ -8,10 +11,82 @@ class FindproductPage extends StatefulWidget {
 }
 
 class _FindproductPageState extends State<FindproductPage> {
+  final List<Map<String, dynamic>> products = [
+    {"title": "Peti", "img": "assets/images/product/peti.png", "color": Colors.blue},
+    {"title": "Kue", "img": "assets/images/product/Kue.png", "color": Colors.green},
+    {"title": "Potion", "img": "assets/images/product/Potion.png", "color": Colors.brown},
+    {"title": "Santet", "img": "assets/images/product/Santet.png", "color": Colors.yellow.shade400},
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: Column(children: [
-      
-    ],));
+    return SafeArea(
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Find Products',
+                style: GoogleFonts.poppins(
+                  fontSize: 16,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+          const Gap(30),
+          _searchBar(),
+          const Gap(20),
+          Expanded(
+            child: SizedBox(
+              width: 364,            
+              child: GridView.builder(
+                itemCount: products.length,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2, // grid 2 kolom
+                  crossAxisSpacing: 15,
+                  mainAxisSpacing: 15,                  
+                ),
+                itemBuilder: (context, index) {
+                  return BatchProduct(
+                    title: products[index]["title"],
+                    img: products[index]["img"],
+                    coloring: products[index]["color"],
+                  );
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Container _searchBar() {
+    return Container(
+      width: 364,
+      height: 52,
+      padding: EdgeInsets.symmetric(horizontal: 12),
+      decoration: BoxDecoration(
+        color: Color(0xFFF2F3F2),
+        borderRadius: BorderRadius.circular(14),
+      ),
+      child: Row(
+        children: [
+          Icon(Icons.search, color: Color.fromARGB(255, 0, 0, 0)),
+          SizedBox(width: 8),
+          Expanded(
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: "Search item...",
+                border: InputBorder.none,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
---
+++ lib/widget/widget_batchProduct.dart
@@ -0,0 +1,45 @@
+// ignore_for_file: file_names, deprecated_member_use
+
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+
+class BatchProduct extends StatelessWidget {
+  final String title;
+  final String img;
+  final Color coloring;
+
+  const BatchProduct({super.key, required this.title, required this.img, required this.coloring});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 175,
+      height: 190,
+      decoration: BoxDecoration(
+        color: coloring.withOpacity(0.3),
+        borderRadius: BorderRadius.circular(16),
+        border: Border.all(color: coloring, width: 1.5),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: 80,
+            width: 80,
+            child: Image.asset(
+              img,
+              fit: BoxFit.contain,
+              width: double.infinity,
+            ),
+          ),
+          const Gap(20),
+          Text(
+            title,
+            style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+          ),
+        ],
+      ),
+    );
+  }
+}
